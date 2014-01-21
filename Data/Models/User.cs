87807bc63e6578312ad5600f49dc64b64e22cfa9
livelyjp <jonny@jonnypillar.co.uk>
2014-01-21 15:54:56 +0000

    Creation of Revised Password Hasher and Security Util

--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CTDataGenerator.Utils;
+using System;
 
 namespace CTDataGenerator.Data
 {
     public partial class User
     {
+        /// <summary>
+        /// User Constructor
+        /// </summary>
+        /// <param name="userID">User ID</param>
+        /// <param name="dateOfBirth">Date Of Birth</param>
+        /// <param name="gender">Gender</param>
+        /// <param name="password">Password String</param>
+        public User(int userID, DateTime dateOfBirth, int gender, string password)
+        {
+            this.UserID = userID;
+            this.DateOfBirth = dateOfBirth;
+            this.Gender = Convert.ToSByte(gender);
+            PasswordHasher passwordHasher = new PasswordHasher(password);
+            this.PasswordHash = passwordHasher.PasswordHash;
+            this.PasswordSalt = passwordHasher.PasswordSalt;
+            this.AdminInt = 0;
+            this.CreationTimestamp = DateTime.Now;
+        }
+
+        /// <summary>
+        /// User Constructor
+        /// </summary>
+        /// <param name="userID">User ID</param>
+        /// <param name="dateOfBirth">Date Of Birth</param>
+        /// <param name="gender">Gender</param>
+        /// <param name="password">Password String</param>
+        /// <param name="admin">Admin Account</param>
+        public User(int userID, DateTime dateOfBirth, int gender, string password, bool admin)
+        {
+            this.UserID = userID;
+            this.DateOfBirth = dateOfBirth;
+            this.Gender = Convert.ToSByte(gender);
+            PasswordHasher passwordHasher = new PasswordHasher(password);
+            this.PasswordHash = passwordHasher.PasswordHash;
+            this.PasswordSalt = passwordHasher.PasswordSalt;
+            this.AdminInt = Convert.ToSByte(admin);
+            this.CreationTimestamp = DateTime.Now;
+        }
     }
 }
---
+++ Utils/SecurityUtil.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Security;
+
+namespace CTDataGenerator.Utils
+{
+    public class SecurityUtil
+    {
+        /// <summary>
+        /// Add Auth Cookie For User
+        /// </summary>
+        /// <param name="userID">User ID</param>
+        public static void AddAuthCookieForUser(string userID)
+        {
+            FormsAuthentication.SetAuthCookie(userID, true);
+        } 
+
+        /// <summary>
+        /// Is a paassword entered by a user valid
+        /// </summary>
+        /// <param name="usersPasswordHash">Password Hash From DB</param>
+        /// <param name="usersPasswordSalt">Password Salt From DB</param>
+        /// <param name="passwordStringToCheck">Password Entered By User</param>
+        /// <returns>Valid Password?</returns>
+        public static bool IsPasswordValid(string passwordStringToCheck, string usersPasswordSalt, string usersPasswordHash)
+        {
+            PasswordHasher passwordHasher = new PasswordHasher(passwordStringToCheck, usersPasswordSalt);
+            return passwordHasher.PasswordHash.Equals(usersPasswordHash);
+        }
+    }
+}
